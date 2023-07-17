441ac4c6c4a4ea0721dbcf7b7afb0ef70e230288
umt.eng52@gmail.com <umt.eng52@gmail.com>
2023-07-17 17:40:14 +0700

    app first

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+$ docker-compose up -d --build
+
+- d หรือ daemon คือการสั่งรัน docker container ใน mode background นะครับ
+-- build คือคำสั่งสำหรับการ pull image เพื่อที่จะสร้าง image สำหรับการนำมาสร้าง container ส่วนใหญ่จะใช้ เฉพาะเวลาเราต้อง build image ใหม่นะครับ
---
+++ phatology-app/database/migrations/2023_07_17_041855_create_phatology_reports_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phatology_reports', function (Blueprint $table) {
+            $table->id()->comment('Surgical number หมายเลขการผ่าตัด');
+            $table->string('lab_order_number')->nullable()->comment('lab_order_number จาก Hosxp')->index();
+            $table->string('hn')->nullable()->comment('HN')->index();
+            $table->string('name')->nullable()->comment('ชื่อ');
+            $table->string('lastname')->nullable()->comment('นามสกุล');
+            $table->integer('age')->nullable()->comment('อายุ');
+            $table->string('gender')->nullable()->comment('เพศ');
+            $table->dateTime('collected_at')->nullable()->comment('วันที่เก็บสิ่งส่งตรวจ');
+            $table->dateTime('received_at')->nullable()->comment('วันที่ได้รับสิ่งส่งตรวจ');
+            $table->string('physician')->nullable()->comment('แพทย์ผู้สั่งตรวจ');
+            $table->string('clinical_history')->nullable()->comment('ประวัติทางคลินิก');
+            $table->string('clinical_diagnosis')->nullable()->comment('การวินิจฉัยทางคลินิก');
+            $table->string('phatology_diag')->nullable()->comment('การวินิจฉัยทางพยาธิวิทยา');
+            $table->string('gross_examination')->nullable()->comment('การตรวจสอบขั้นต้น');
+            $table->string('gross_examiner')->nullable()->comment('ผู้ตรวจสอบขั้นต้น');
+            $table->timestamps()->comment('วันที่สร้าง');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phatology_reports');
+    }
+};
