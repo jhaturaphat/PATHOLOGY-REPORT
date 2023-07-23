1068e77fb55279551fe41de3c629c4aff9689a24
jhaturaphat <umt.eng52@gmail.com>
2023-07-23 19:59:50 +0700

    delete plugins

--- pathology-app/resources/views/pathology-a/image1.blade.php
+++ pathology-app/resources/views/pathology-a/image1.blade.php
@@ -1,10 +1,17 @@
 <page size="A4" id="image1">
     <div class="container">
         <div class="page-number">1</div>
-        @include('pathology-a.header') 
-        <div class="container">
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-        </div>
+        @include('pathology-a.header')         
+            <section>
+                <span><b>CLINICAL HISTORY:</b></span>
+                <textarea name="" id="" cols="30" rows="5">                    is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</textarea>
+            </section>
+            <section>
+                <span><b>CLINICAL DIAGNOSIS:</b></span>
+                <textarea name="" id="" cols="30" rows="5">                      </textarea>
+            </section>
+            
+        
         <div id="editor"></div>   
     </div>  
 </page>
--- pathology-app/resources/views/pathology-a/index.blade.php
+++ pathology-app/resources/views/pathology-a/index.blade.php
@@ -18,28 +18,14 @@
     <button id="release" style="position: fixed; top:0; left:0; z-index:100">Release</button>
 
     <script src="{{asset('js/pathology-a.js')}}"></script>
-    <script src="{{asset('js/plugins/ckeditor/ckeditor.js')}}"></script>
-    <script src="https://cdn.tiny.cloud/1/dcgwk7e1pfejbm42161axxt4cxm5sxzoj8jwryxerpo92i0q/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/38.1.1/classic/ckeditor.js"></script>
 
     <script>        
-        CKEDITOR.replace( 'editor', {
-            // extraPlugins: 'uicolor,colorbutton,colordialog,font',
-        } );
-        CKEDITOR.addCss(".cke_editable { cursor:text; font-size: 25px; font-family: 'Roboto', sans-serif;  color: #FFFFFF;background-color: #006991;}");
+        ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
+            } );
     </script>
-
-<script>
-    tinymce.init({
-      selector: 'textarea',
-      plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
-      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-      tinycomments_mode: 'embedded',
-      tinycomments_author: 'Author name',
-      mergetags_list: [
-        { value: 'First.Name', title: 'First Name' },
-        { value: 'Email', title: 'Email' },
-      ]
-    });
-  </script>
 </body>
 </html>
